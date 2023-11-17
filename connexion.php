9e28978682f08f78490216b7cb5d939323619928
Karo-ssr <112403897+Karo-ssr@users.noreply.github.com>
2023-11-17 09:22:17 +0100

    creation du php de conexion
    creation du php de conexion

---
+++ connexion.php
@@ -0,0 +1,27 @@
+<?php
+
+$pdo = new PDO(
+    "MySQL:host=localhost;
+    BDName = fait_ton_cine; charset = UTF8",
+    "maka",
+    "maka1"
+);
+
+var_dump($pdo);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Page De Connexion</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
