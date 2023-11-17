704f0b38562a208faba4eecfd4d1f35b43d42619
Karo-ssr <112403897+Karo-ssr@users.noreply.github.com>
2023-11-17 11:02:07 +0100

    Update connexion.php

--- connexion.php
+++ connexion.php
@@ -9,3 +9,15 @@ $pdo = new PDO(
 
 var_dump($pdo);
 
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    jjjjjj
+</body>
+</html>
