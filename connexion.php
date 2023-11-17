1328fe426499920e177000b597096b61b8a45617
Karo-ssr <112403897+Karo-ssr@users.noreply.github.com>
2023-11-17 11:39:39 +0100

    bonjour future moi
    bonjour future moi au revoir

--- connexion.php
+++ connexion.php
@@ -1,23 +1,13 @@
-<?php
+<?php 
 
-$pdo = new PDO(
-    "MySQL:host=localhost;
-    BDName=fait_ton_cine; charset=UTF8",
-    "maka",
-    "maka1"
-);
+$pdo = new PDO("mysql:host=localhost;dbname=fait_ton_cine;charset=UTF8", "maka","maka1");
 
-var_dump($pdo);
+$statement = $pdo->prepare("SELECT * FROM films");
 
-?>
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    jjjjjj
-</body>
-</html>
+$statement->execute();
+
+$res = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+
+
+var_dump($res);
